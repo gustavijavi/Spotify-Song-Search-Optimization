b3705c1d30f35dc741f0a631af1dd33728e7d8cb
gustavijavi <javier.collroman@gmail.com>
2026-03-12 15:47:30 -0400

    Red-Black.cpp initial commit Added basic functions with no implementation as said in #3

---
+++ src/RedBlackTree.cpp
@@ -0,0 +1,31 @@
+#include "RedBlackTree.h"
+
+RedBlackTree::Node::Node(const string& songTitle, const string& artistName, int releaseYear, int popularity){
+    
+    // TODO: implement
+
+}
+
+RedBlackTree::RedBlackTree(){
+
+    // TODO: implement
+
+}
+
+RedBlackTree::~RedBlackTree(){
+    
+    // TODO: implement
+
+}
+
+void RedBlackTree::searchBySongTitle(const string& songTitle){
+
+    // TODO: implement
+
+}
+
+void RedBlackTree::searchByArtistName(const string& artistName){
+    
+    // TODO: implement
+
+}
