5dcacdc3d74db072b5a350c282a5c5410ed76632
mbkilroy83 <matt.kilroy83@gmail.com>
2026-03-20 14:51:13 -0600

    feat: implemented Trie insert and search. (Issues #18, #20)

--- src/TrieTree.h
+++ src/TrieTree.h
@@ -7,14 +7,14 @@ struct Metadata
 {
     string _songTitle;
     string _artistName;
-    int _releaseYear;
-    int _popularity;
-    Metadata(string songTitle, string artistName, int releaseYear, int popularity);
+    string _releaseYear;
+    string _popularity;
+    Metadata(const string& songTitle, const string& artistName, const string& releaseYear, const string& popularity);
 };
 
 struct TrieNode
 {
-    unordered_map<char, TrieNode*> _children;
+    unordered_map<char32_t, TrieNode*> _children;
     bool _isSongTitle;
     Metadata* _metadata;
     TrieNode();
@@ -23,9 +23,10 @@ struct TrieNode
 class TrieTree
 {
     TrieNode* _root;
-
+    void deleteNode(const TrieNode* node);
+public:
     TrieTree();
     ~TrieTree();
-    void insert(const string& songTitle, string artistName, int releaseYear, int popularity);
-    bool search(const string& songTitle);
+    void insert(const string& songTitle, const string& artistName, const string& releaseYear, const string& popularity) const;
+    Metadata* search(const string& songTitle) const;
 };
