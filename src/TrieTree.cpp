dbe051f4cad17aa719db547f3e9c58cf68558cb3
mbkilroy83 <matt.kilroy83@gmail.com>
2026-03-25 09:31:39 -0600

    documentation: documented TrieTree.cpp and TrieTree.h (issue #30)

--- src/TrieTree.cpp
+++ src/TrieTree.cpp
@@ -1,7 +1,6 @@
 #include "TrieTree.h"
-#include <iostream>
 
-
+//Define Metadata constructor
 Metadata::Metadata(const string& songTitle, const string& artistName, const string& releaseYear, const string& popularity)
 {
     _songTitle = songTitle;
@@ -10,56 +9,61 @@ Metadata::Metadata(const string& songTitle, const string& artistName, const stri
     _popularity = popularity;
 }
 
+//Define Trie node constructor
 TrieNode::TrieNode()
 {
-    _isSongTitle = false;
-    _metadata = nullptr;
+    _isSongTitle = false;                   //Indicates that node is end of song and holds metadata
+    _metadata = nullptr;                    //Pointer to metadata struct
 }
 
+//Define TrieTree constructor
 TrieTree::TrieTree()
 {
-    _root = new TrieNode();
+    _root = new TrieNode();                 //Creates empty root node automatically
 }
 
+//Define TrieTree destructor
 TrieTree::~TrieTree()
 {
     deleteNode(_root);
 }
 
+//Recursively delete all nodes
 void TrieTree::deleteNode(const TrieNode* node)
 {
-    if (!node)
+    if (!node)                                                          //If pointer is null return
         return;
-    for (auto& child: node->_children)
-    {
+    for (auto& child: node->_children)    //Traverse all child nodes
         deleteNode(child.second);
-    }
-    delete node->_metadata;
-    delete node;
+
+    delete node->_metadata;                                             //Delete metadata
+    delete node;                                                        //Delete node
 }
 
+//Insert song title and metadata into Trie
 void TrieTree::insert(const string& songTitle, const string& artistName, const string& releaseYear, const string& popularity) const
 {
-    TrieNode* current = _root;
-    for (char32_t letter : songTitle)
+    TrieNode* current = _root;                                                              //Start at root
+    for (char32_t letter : songTitle)                                                       //For each character in song title
     {
-        if (current->_children.find(letter) == current->_children.end())
-            current->_children[letter] = new TrieNode();
+        if (current->_children.find(letter) == current->_children.end())                    //If character doesnt exist in branch
+            current->_children[letter] = new TrieNode();                                    //Create new node with character
 
-        current = current->_children[letter];
+        current = current->_children[letter];                                               //Traverse to next character
     }
-    current->_isSongTitle = true;
-    current->_metadata = new Metadata(songTitle, artistName, releaseYear, popularity);
+    current->_isSongTitle = true;                                                           //At end of song title set flag
+    current->_metadata = new Metadata(songTitle, artistName, releaseYear, popularity);      //Insert metadata
 }
 
+//Search Trie for song title and return metadata
 Metadata* TrieTree::search(const string& songTitle) const
 {
-    TrieNode* current = _root;
-    for (char32_t letter : songTitle)
+    TrieNode* current = _root;                                                              //Start at root
+    for (char32_t letter : songTitle)                                                       //For each character in song title
     {
-        if (current->_children.find(letter) == current->_children.end())
-            return nullptr;
-        current = current->_children[letter];
+        if (current->_children.find(letter) == current->_children.end())                    //If character doesnt exist in branch
+            return nullptr;                                                                 //Return nullptr
+        current = current->_children[letter];                                               //Traverse to next character
     }
-    return current->_metadata;
+    return current->_metadata;                                                              //At end of song title return metadata
 }
--- src/TrieTree.h
+++ src/TrieTree.h
@@ -3,6 +3,7 @@
 #include <unordered_map>
 using namespace std;
 
+//Define Metadata struct
 struct Metadata
 {
     string _songTitle;
@@ -12,14 +13,16 @@ struct Metadata
     Metadata(const string& songTitle, const string& artistName, const string& releaseYear, const string& popularity);
 };
 
+//Define TrieNode struct
 struct TrieNode
 {
-    unordered_map<char32_t, TrieNode*> _children;
+    unordered_map<char32_t, TrieNode*> _children;       //Unordered map with char32_t keys used for special characters
     bool _isSongTitle;
     Metadata* _metadata;
     TrieNode();
 };
 
+//Define TrieTree class
 class TrieTree
 {
     TrieNode* _root;
