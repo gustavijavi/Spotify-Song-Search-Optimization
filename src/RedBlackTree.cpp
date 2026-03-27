13ef5d568f87e74d16779afb32afb34f4adbfbd2
Jayesh Agarwal <jayesh100100@gmail.com>
2026-03-27 02:33:49 -0400

    Fixed a few logic issues and added some comments

--- src/RedBlackTree.cpp
+++ src/RedBlackTree.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-//Definition for RedBlackTree Metadata constructor
+//Definition for RedBlackTree Data constructor
 Data::Data(const string& songTitle, const string& artistName, const string& releaseYear, const string& popularity)
 {
     _songTitle = songTitle;
@@ -12,22 +12,26 @@ Data::Data(const string& songTitle, const string& artistName, const string& rele
     _popularity = popularity;
 }
 
+//Definition for RBTreeNode constructor
 RBTreeNode::RBTreeNode()
 {
     isBlack = true;
     _data = nullptr;
 }
 
+//Definition for RedBlackTree constructor
 RedBlackTree::RedBlackTree()
 {
     _root = new RBTreeNode();
 }
 
+//Definition for RedBlackTree deconstructor
 RedBlackTree::~RedBlackTree()
 {
     deleteNode(_root);
 }
 
+//Song delete function for RedBlackTree deconstructor
 void RedBlackTree::deleteNode(const RBTreeNode* node) {
     if (node == nullptr) {
         return;
@@ -40,6 +44,7 @@ void RedBlackTree::deleteNode(const RBTreeNode* node) {
     delete node;
 }
 
+//Song insert function for RedBlackTree
 void RedBlackTree::insert(const string& songTitle, const string& artistName, const string& releaseYear, const string& popularity) const
 {
     if (_root->_data == nullptr) {
@@ -51,24 +56,31 @@ void RedBlackTree::insert(const string& songTitle, const string& artistName, con
     bool inserting = true;
 
     while (inserting) {
-        if (current->leftChild->_data == nullptr) {
+        if (current->leftChild->_data == nullptr || current->rightChild->_data == nullptr) {
             if (current->isBlack) {
-                current->leftChild->_data = new Data(songTitle, artistName, releaseYear, popularity);
-                current->leftChild->isBlack = false;
-                current->leftChild->parent = current;
+                if (current->_data->_songTitle.compare(songTitle) > 0) {
+                    current->leftChild->_data = new Data(songTitle, artistName, releaseYear, popularity);
+                    current->leftChild->isBlack = false;
+                    current->leftChild->parent = current;
+                    inserting = false;
+                }
+                else {
+                    current->rightChild->_data = new Data(songTitle, artistName, releaseYear, popularity);
+                    current->rightChild->isBlack = false;
+                    current->rightChild->parent = current;
+                    inserting = false;
+                }
             }
             else {
-                //fix/balance tree code
+                //code to fix or balance tree
             }
         }
-        else if (current->rightChild->_data == nullptr) {
-            if (current->isBlack) {
-                current->rightChild->_data = new Data(songTitle, artistName, releaseYear, popularity);
-                current->rightChild->isBlack = false;
-                current->rightChild->parent = current;
+        else {
+            if (current->_data->_songTitle.compare(songTitle) > 0) {
+                current = current->leftChild;
             }
             else {
-                //fix/balance tree code
+                current = current->rightChild;
             }
         }
     }
@@ -78,22 +90,19 @@ void RedBlackTree::insert(const string& songTitle, const string& artistName, con
 Data* RedBlackTree::search(const string& songTitle) const
 {
     RBTreeNode* current = _root;
-    bool searching = true;
-    while (searching) {
-        string currentSongTitle = current->_data->_songTitle;
-
+    while (true) {
         if (current->_data == nullptr) {
-            return nullptr;
+            return nullptr; //nothing is returned if database is empty
         }
 
-        if (currentSongTitle.compare(songTitle) < 0) {
-            current = current->rightChild;
+        if (current->_data->_songTitle.compare(songTitle) < 0) {
+            current = current->rightChild; //Goes to right child to search for song title
         }
-        else if (currentSongTitle.compare(songTitle) > 0) {
-            current = current->leftChild;
+        else if (current->_data->_songTitle.compare(songTitle) > 0) {
+            current = current->leftChild; //Goes to left child to search for song title
         }
         else {
-            return current->_data;
+            return current->_data; //Song title and other data returned
         }
     }
 }
